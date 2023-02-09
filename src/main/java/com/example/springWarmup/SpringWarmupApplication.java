8e3ac2c3b80543233b3e2aac0b6c34f5086a60d4
ErnestJo <smartlabstech@yahoo.com>
2023-02-09 14:58:48 +0300

    first commit

---
+++ src/main/java/com/example/springWarmup/SpringWarmupApplication.java
@@ -0,0 +1,13 @@
+package com.example.springWarmup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWarmupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWarmupApplication.class, args);
+	}
+
+}
