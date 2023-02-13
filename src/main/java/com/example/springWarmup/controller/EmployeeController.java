d786449eb8f6c002a69a514bb68f5f1b0afbbf35
ErnestJo <smartlabstech@yahoo.com>
2023-02-13 16:46:47 +0300

    get mapping && changing application.properites

--- src/main/java/com/example/springWarmup/controller/EmployeeController.java
+++ src/main/java/com/example/springWarmup/controller/EmployeeController.java
@@ -1,29 +1,32 @@
 package com.example.springWarmup.controller;
 
-import ch.qos.logback.core.model.conditional.ElseModel;
 import com.example.springWarmup.model.Employee;
-import com.example.springWarmup.repository.EmployeeRepository;
 import com.example.springWarmup.service.EmployeeService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
-public class EmployeeContoller {
+public class EmployeeController {
     private EmployeeService employeeService;
 
-    public EmployeeContoller(EmployeeService employeeService) {
+    public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
+
+    // build create employee REST API
     @PostMapping()
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
         return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public List<Employee>  gerEmployee(){
+        return employeeService.getAllEmployees();
     }
 
 }
