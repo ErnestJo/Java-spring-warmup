a3e1d8f2c2c7d223da09f2dd7bd7be3f51ffd785
ErnestJo <smartlabstech@yahoo.com>
2023-02-14 09:11:29 +0300

    Delete controller

--- src/main/java/com/example/springWarmup/controller/EmployeeController.java
+++ src/main/java/com/example/springWarmup/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Struct;
 import java.util.List;
 
 @RestController
@@ -24,9 +25,33 @@ public class EmployeeController {
         return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
     }
 
+    //build get employee REST API
     @GetMapping()
     public List<Employee>  gerEmployee(){
         return employeeService.getAllEmployees();
     }
 
+    // build get employee by ID REST API
+    // http://localhost:8080/api/employee/1
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id")  long EmployeeId){
+    return  new ResponseEntity<Employee>(employeeService.getSingleEmployee(EmployeeId), HttpStatus.OK);
+    }
+
+
+    // build update employee REST API
+    // http://localhost:8080/api/employee/1
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id")  long id, @RequestBody Employee employee){
+    return new ResponseEntity<Employee>(employeeService.updateEmployee(employee,id), HttpStatus.OK);
+    }
+
+    // build update employee REST API
+    // http://localhost:8080/api/employee/1
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+    employeeService.deleteEmployee(id);
+
+    return new ResponseEntity<String>("Employee deleted successful!.", HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/springWarmup/service/EmployeeService.java
+++ src/main/java/com/example/springWarmup/service/EmployeeService.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
     List<Employee>  getAllEmployees();
+    Employee getSingleEmployee(long id);
+    Employee updateEmployee(Employee employee, long id);
+    void  deleteEmployee(long id);
 }
